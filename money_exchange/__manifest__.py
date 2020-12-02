5735581b0bba5907aa102dec5684f9c4a7696d71
nunsophanon <sophanonnun07@gmail.com>
2020-12-02 10:03:22 +0000

    Installable

--- money_exchange/__manifest__.py
+++ money_exchange/__manifest__.py
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Nun Sophanon, ERP CAMBODIA",
+    'website': "https://www.erpcambodia.biz",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales/Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,4 +32,6 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'auto_install': False
 }
