43d1ebfb5fbba3ba0301313ba2ed7af3eb2bb28a
nunsophanon <sophanonnun07@gmail.com>
2020-12-05 03:10:22 +0000

    change model

--- money_exchange/__manifest__.py
+++ money_exchange/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "money_exchange",
+    'name': "Money Exchange",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
--- money_exchange/models/.ipynb_checkpoints/models-checkpoint.py
+++ money_exchange/models/.ipynb_checkpoints/models-checkpoint.py
@@ -4,20 +4,22 @@ from odoo import models, fields, api
 
 
 class money_exchange(models.Model):
-    _name = 'money_exchange.money_exchange'
-    _description = 'money_exchange.money_exchange'
+    _name = 'money.exchange'
+    _description = 'money.exchange'
     
     @api.depends('input_currency_id', 'date_change')
     def _compute_input_currency_rate(self):
         if self.input_currency_id:
-            self.input_currency_rate = self.input_currency_id.with_context(date=self.date_change).rate or 1.0
+            self.input_currency_rate = self.input_currency_id.rate or 1.0
+            # self.input_currency_rate = self.input_currency_id.with_context(date=self.date_change).rate or 1.0
         else:
             self.input_currency_rate = 1.0
             
     @api.depends('output_currency_id', 'date_change')
     def _compute_output_currency_rate(self):
         if self.output_currency_id:
-            self.output_currency_rate = self.output_currency_id.with_context(date=self.date_change).rate or 1.0
+            self.output_currency_rate = self.output_currency_id.rate or 1.0
+            # self.output_currency_rate = self.output_currency_id.with_context(date=self.date_change).rate or 1.0
         else:
             self.output_currency_rate = 1.0
             
@@ -30,38 +32,41 @@ class money_exchange(models.Model):
         """
         self.output_amount = (self.input_amount*self.output_currency_rate)/self.input_currency_rate
 
-    name = fields.Char(string='Reference', required=True, copy=False, readonly=True, states={'draft': [('readonly', False)]}, index=True, default=lambda self: ('New'))
     state = fields.Selection([
         ('draft', 'Draft'),
-        ('computed', 'Computed'),
+        ('done', 'Done'),
         ('cancel', 'Cancelled'),
-        ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+    ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+    name = fields.Char(string='Reference', required=True, copy=False, readonly=True, states={'draft': [('readonly', False)]}, index=True, default=lambda self: ('New'))
+
     date_change = fields.Datetime(string='Date', required=True, readonly=True, index=True, states={'draft': [('readonly', False)]}, copy=False, default=fields.Datetime.now, help="Creation date of draft/sent orders,\nConfirmation date of confirmed orders.")
     user_id = fields.Many2one(
-        'res.users', string='Salesperson', index=True, tracking=2, default=lambda self: self.env.user)
-    company_id = fields.Many2one('res.company', 'Company', required=True, index=True, default=lambda self: self.env.company)
+        'res.users', string='Salesperson', index=True, tracking=2, readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.user)
+    company_id = fields.Many2one('res.company', 'Company', required=True, index=True, readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.company)
     partner_id = fields.Many2one(
         'res.partner', string='Customer', readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+        states={'draft': [('readonly', False)]},
         required=True, change_default=True, index=True, tracking=1,
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",)
-    input_currency_id = fields.Many2one('res.currency', store=True, string='Input Currency')
-    output_currency_id = fields.Many2one('res.currency', store=True, string='Output Currency')
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
+    input_currency_id = fields.Many2one('res.currency', store=True, string='Input Currency', readonly=True, states={'draft': [('readonly', False)]})
+    output_currency_id = fields.Many2one('res.currency', store=True, string='Output Currency', readonly=True, states={'draft': [('readonly', False)]})
     input_currency_rate = fields.Float("Input Currency Rate", compute='_compute_input_currency_rate', compute_sudo=True, store=True, digits=(12, 6), readonly=True, help='Input Rate')
     output_currency_rate = fields.Float("Output Currency Rate", compute='_compute_output_currency_rate', compute_sudo=True, store=True, digits=(12, 6), readonly=True, help='Output Rate')
-    input_amount = fields.Float(string='Input Amount',store=True)
+    input_amount = fields.Float(string='Input Amount',store=True, readonly=True, states={'draft': [('readonly', False)]})
 #     commission = fields.Float(string='Commission',store=True)
     output_amount = fields.Float(string='Output Amount', store=True, readonly=True, compute='_compute_output_amount', tracking=4)
     
     note = fields.Text('Note')
-    
-    
-    def _action_compute(self):
-        self.write({'state': 'computed'})
-        
-    def _action_cancel(self):
-        self.write({'state': 'cencelled'})
-        
-    def _action_draft(self):
+
+
+    def action_done(self):
+        seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(self.date_change))
+        name = self.env['ir.sequence'].next_by_code('money.exchange', sequence_date=seq_date) or ('New')
+        self.write({'state': 'done','name':name})
+
+    def action_cancel(self):
+        self.write({'state': 'cancel'})
+
+    def action_draft(self):
         self.write({'state': 'draft'})
 
